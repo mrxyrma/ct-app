08d2dd765948e0a000db296b1e04122d64831b5d
Андрей Королев <ander_kor@mail.ru>
2024-02-29 16:05:03 +0300

    feat: настроить поддержку PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,35 @@
 import vue from '@vitejs/plugin-vue';
 import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
+
+const vitePWA = VitePWA({
+  registerType: 'autoUpdate',
+  manifest: {
+    name: 'Подбор оборудования',
+    short_name: 'Таблицы',
+    description: 'Приложение по подбору оборудования для сотрудников ГК Сети-Телеком',
+    lang: 'ru',
+    icons: [
+      {
+        src: 'assets/icons/android-chrome-192x192.png',
+        sizes: '192x192',
+        type: 'image/png',
+      },
+      {
+        src: 'assets/icons/android-chrome-512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
+      },
+    ],
+    theme_color: '#ffffff',
+    background_color: '#ffffff',
+    display: 'standalone',
+  },
+});
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), vitePWA],
   resolve: {
     alias: {
       src: '/src',
